de9195da6c5bb64dfae93298a9fcf32cdb1f667d
Kirill Grabar <kirill.grabar4@gmail.com>
2026-02-08 23:00:02 +0100

    fixed: radius can be 0

--- peer/peer.go
+++ peer/peer.go
@@ -17,12 +17,16 @@ type Peer struct {
 	ConnectionRadius int //simulating connection radius
 	Name string
 	MaxShareSpeedPerSec int	//simulating max share speed in kb/s
+	ConnectedTo []string
 }
 
 func NewPeer(name string) *Peer {
 	id := uuid.New();
 	randomSpeed := rand.IntN(50)
-	randomRadius := rand.IntN(10)
+	randomRadius := rand.IntN(5)
+	if randomRadius < 1 {
+		randomRadius = 1
+	}
 	if randomSpeed < 10 {
 		randomSpeed = 10
 	}
