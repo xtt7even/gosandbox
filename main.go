3c7ed56faf6ca1bddf4f156a9a75fe9f81059cee
Kirill Grabar <kirill.grabar4@gmail.com>
2026-02-08 18:09:54 +0100

    feat: simple console map draw (not sure if accurate and efficient enough yet)

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"math/rand/v2"
 	simmap "sandbox/sandbox/map"
 	simplego "sandbox/sandbox/mobility"
+	"sandbox/sandbox/painter"
 	"sandbox/sandbox/peer"
 	"time"
 )
 
 func main() {
-	simmap := simmap.NewMap(250)
+	simmap := simmap.NewMap(50)
 	names := [5]string{"Alice", "John", "Matthew", "Gregory", "Maria"}
 	var peers []peer.Peer
 	for i := 0; i < len(names); i++ {
@@ -34,16 +34,20 @@ func tick(peers []peer.Peer, m *simmap.Map) {
 			simplego.PickRandomWaypointForPeer(peer, m)
 		}
 		simplego.SimpleMove(peer)
+		painter.Draw(m, peers)
 
-		//? temp
-		if i == 0 {
-			fmt.Println("========================================")
-			fmt.Println("Tick:", time.Now().Format("2006-01-02 15:04:05"))
-			fmt.Printf("%-3s %-12s %-20s %-20s %s\n", "ID", "NAME", "POSITION", "WAYPOINT", "DETAILS")
-		}
-		p := peers[i]
-		pos := fmt.Sprintf("pos=%v,%v", p.X, p.Y)
-		wp := fmt.Sprintf("wp=%v,%v", p.WaypointX, p.WaypointY)
-		fmt.Printf("%-3d %-12s %-20s %-20s %v\n", i, p.Name, pos, wp, p)
+		time.Sleep(50 * time.Millisecond)
 	}
 }
+
+// func debugOutput (p peer.Peer) {
+// 	if i == 0 {
+// 		fmt.Println("========================================")
+// 		fmt.Println("Tick:", time.Now().Format("2006-01-02 15:04:05"))
+// 		fmt.Printf("%-3s %-12s %-20s %-20s\n", "ID", "NAME", "POSITION", "WAYPOINT")
+// 	}
+// 	p := peers[i]
+// 	pos := fmt.Sprintf("pos=%v,%v", p.X, p.Y)
+// 	wp := fmt.Sprintf("wp=%v,%v", p.WaypointX, p.WaypointY)
+// 	fmt.Printf("%-3d %-12s %-20s %-20s\n", i, p.Name, pos, wp)
+// }
--- painter/painter.go
+++ painter/painter.go
@@ -1,14 +1,47 @@
 package painter
 
 import (
+	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
 	simmap "sandbox/sandbox/map"
 	"sandbox/sandbox/peer"
 )
 
-func Draw(m *simmap.Map, peers *[]peer.Peer) {
-	//TODO
-	// for y := 0; y < m.Size; y++ {
-	// 	for x := 0; x < m.Size; x++ {
-	// 	}
-	// }
+func Draw(m *simmap.Map, peers []peer.Peer) {
+	clearScreen()
+	points := make([][2]int, len(peers))
+
+	for i := 0; i < len(peers); i++ {
+		points[i][0] = peers[i].X
+		points[i][1] = peers[i].Y
+	}
+	for y := 0; y < m.Size; y++ {
+		for x := 0; x < m.Size; x++ {
+			var isPeerCoord bool
+			for _, peerCoord := range points {
+				if (peerCoord[0] == x && peerCoord[1] == y) {
+					isPeerCoord = true
+				}
+			}
+			if isPeerCoord {
+				fmt.Print("o")
+			} else {
+				fmt.Print(" ")
+			}
+		}
+		fmt.Println()
+	}
+}
+
+func clearScreen() {
+    var cmd *exec.Cmd
+    if runtime.GOOS == "windows" {
+        cmd = exec.Command("cmd", "/c", "cls")
+    } else {
+        cmd = exec.Command("clear")
+    }
+    cmd.Stdout = os.Stdout
+    cmd.Run() 
 }
